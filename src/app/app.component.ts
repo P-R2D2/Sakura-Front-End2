93268dfe8b66e7143c1a1dd500a76b5bc01b4716
Luis Eduardo <pplpierre20@gmail.com>
2025-06-15 23:15:25 -0300

    Sakura2 novo projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {MatTooltip} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton, RouterLink, MatTooltip],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {FilmFormComponent} from './films/film-form/film-form.component';
+import {FilmListComponent} from './films/film-list/film-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {'path': 'list', component: FilmListComponent},
+  {'path': 'form', component: FilmFormComponent},
+
+];
