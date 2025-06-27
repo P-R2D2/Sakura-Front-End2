00b4de13ea98d1cad5ce3f774f8b09baa3ddc9d8
Luis Eduardo <pplpierre20@gmail.com>
2025-06-26 22:12:44 -0300

    Adicionando rota na home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import {NavigationEnd, Router, RouterLink, RouterOutlet} from '@angular/router';
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatIcon} from '@angular/material/icon';
-import {MatIconButton} from '@angular/material/button';
+import {MatAnchor, MatIconButton} from '@angular/material/button';
 import {MatTooltip} from '@angular/material/tooltip';
+import {filter} from 'rxjs';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton, RouterLink, MatTooltip],
+  imports: [RouterOutlet, MatToolbar, MatIcon, MatIconButton, RouterLink, MatTooltip, MatAnchor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Sakura Stream';
+
+  hideLinks = false;
+
+  constructor(private router: Router) {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        const hiddenRoutes = ['/list', '/form'];
+        this.hideLinks = hiddenRoutes.includes(event.urlAfterRedirects);
+      });
+  }
 }
